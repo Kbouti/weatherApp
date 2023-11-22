6e8e8acac950d02d258e639201a4909739f8cd31
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-11-22 14:45:49 -0800

    Use npm run watch to start and stop watch mode

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log("test"),console.log("shitsnacks from the api.js file")})();
+(()=>{"use strict";console.log("test"),console.log("shitsnack fuckery from the api.js file"),console.log("even more shit fuckery")})();
--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,10 +1,6 @@
-function testApi(){
-    console.log(`shitsnack fuckery from the api.js file`)
-
+function testApi() {
+  console.log(`shitsnack fuckery from the api.js file`);
+  console.log(`even more shit fuckery`);
 }
 
-
-
-export {
-    testApi
-}
+export { testApi };
