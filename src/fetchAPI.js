45cf0f3605e13756ae937df45c216de6a6d3f173
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-11-22 16:02:54 -0800

    Add weather API links

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -3,4 +3,17 @@ function testApi() {
   console.log(`even more shit fuckery`);
 }
 
+// Weather API site: https://www.weatherapi.com/my/
+
+// My weather API key: d261db3fd8fb4676bde201850231611
+
+// Weather API request template: 
+// https://api.weatherapi.com/v1/current.json?key=11111111111111111&q=london
+
+// My weather request for tacoma: 
+// https://api.weatherapi.com/v1/current.json?key=d261db3fd8fb4676bde201850231611&q=tacoma
+
+
+
+
 export { testApi };
