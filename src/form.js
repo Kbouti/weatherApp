1a6b71ca88a1c15c9935a25142b48f03fec6a497
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-11-25 13:46:43 -0800

    Clean up function declarations in form.js

--- src/form.js
+++ src/form.js
@@ -1,44 +1,45 @@
-
 const form = document.getElementById(`form`);
 
-const unitButton = document.getElementById(`unitButton`)
-
+const unitButton = document.getElementById(`unitButton`);
+const cityInput = document.getElementById(`cityInput`);
 
 function addFormListeners() {
   form.addEventListener(`submit`, (e) => {
-    e.preventDefault();
-
-    let cityInput = document.getElementById(`cityInput`);
-
-    let userInput = cityInput.value;
-
-    console.log(`form submitted`);
-    console.log(userInput);
-
-    alert(`You submitted the form`);
-    return userInput;
+    formSubmit(e);
   });
-
-unitButton.addEventListener(`click`, function(){
-    toggleUnits()});
-
-
+  unitButton.addEventListener(`click`, () => {
+    toggleUnits();
+  });
 }
 
-function toggleUnits(){
-    if (unitButton.classList.contains(`farenheit`)){
-        unitButton.classList.remove(`farenheit`);
-        unitButton.classList.add(`celcius`);
-        unitButton.innerHTML = `&#8451;`
-        console.log(`toggle triggered`);
-        return;
-    } else {
-        console.log(`toggle triggered`);
-        unitButton.classList.remove(`celcius`);
-        unitButton.classList.add(`farenheit`);
-        unitButton.innerHTML = `&#8457;`
-        return;
-    }
+function getUserInput() {
+  let userInput = cityInput.value;
+  console.log(userInput);
+  return userInput;
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+  let userInput = getUserInput();
+  getWeather(userInput);
+}
+
+function getWeather(city) {}
+
+function toggleUnits() {
+  if (unitButton.classList.contains(`farenheit`)) {
+    unitButton.classList.remove(`farenheit`);
+    unitButton.classList.add(`celcius`);
+    unitButton.innerHTML = `&#8451;`;
+    console.log(`toggle triggered`);
+    return;
+  } else {
+    console.log(`toggle triggered`);
+    unitButton.classList.remove(`celcius`);
+    unitButton.classList.add(`farenheit`);
+    unitButton.innerHTML = `&#8457;`;
+    return;
+  }
 }
 
 export { addFormListeners };
