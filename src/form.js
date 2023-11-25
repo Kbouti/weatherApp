93b493d30683d2d125f2020800935c08cdab8ea6
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-11-25 13:20:51 -0800

    Use degree symbols for farenheit and celcius

--- src/form.js
+++ src/form.js
@@ -1,7 +1,7 @@
 
 const form = document.getElementById(`form`);
 
-
+const unitButton = document.getElementById(`unitButton`)
 
 
 function addListener() {
@@ -18,6 +18,20 @@ function addListener() {
     alert(`You submitted the form`);
     return userInput;
   });
+}
+
+function toggleUnits(){
+    if (unitButton.classList.contains(`farenheit`)){
+        unitButton.classList.remove(`farenheit`);
+        unitButton.classList.add(`celcius`);
+        unitButton.innerHTML = `&#8451;`
+        return;
+    } else {
+        unitButton.classList.remove(`celcius`);
+        unitButton.classList.add(`farenheit`);
+        unitButton.innerHTML = `&#8457;`
+        return;
+    }
 }
 
 export { addListener };
