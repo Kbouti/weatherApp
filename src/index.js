8ca7e3d5b9ca3586923ec627106c214f90603fd6
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-12-13 19:17:41 -0800

    Add note on import error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 console.log(`index.js has loaded`)
 
 // This is the main js file for development. I can make compartmentalized js files and import their functions here. Webpack will bundle in ./dist/main.js and read from there
-import './style.css'
+// (That's the goal anyway)
+
+// ********************************************************************************
+// Ok, so the following command errors when this css import is present:
+// npx webpack --config webpack.config.js
+import './style.css';
 
 import {
     testApi,
